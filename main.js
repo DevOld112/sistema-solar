f2faa5cde913c9853f343f313ac7adc7402e8aac
DevOld112 <XwilmerX53@gmail.com>
2023-05-06 18:49:50 -0500

    Agregando planetas faltantes, animaciones, ventanas emergente, y musica de fondo

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  const audio = document.getElementById("myAudio");
+  audio.volume = 0.05;
+
+
 // Selecciona el elemento con clase "stars"
 const stars = document.querySelector('.stars');
 
@@ -28,10 +32,12 @@ const cerrarVentanaSol = document.getElementById('cerrar-ventana-sol')
 
 sol.addEventListener('click', () => {
   ventanaEmergenteSol.classList.remove('oculto');
+  ventanaEmergenteSol.classList.add('ventana-emergente');
 })
 
 cerrarVentanaSol.addEventListener('click', () => {
   ventanaEmergenteSol.classList.add('oculto')
+
 })
 
 
@@ -43,6 +49,7 @@ const cerrarVentanaVenus = document.getElementById('cerrar-ventana-venus');
 
 venus.addEventListener('click', () => {
   ventanaEmergenteVenus.classList.remove('oculto');
+  ventanaEmergenteVenus.classList.add('ventana-emergente');
 });
 
 cerrarVentanaVenus.addEventListener('click', () => {
@@ -58,13 +65,134 @@ const cerrarVentanaTierra = document.getElementById('cerrar-ventana-tierra');
 
 tierra.addEventListener('click', () => {
   ventanaEmergenteTierra.classList.remove('oculto')
+  ventanaEmergenteTierra.classList.add('ventana-emergente');
 });
 
 cerrarVentanaTierra.addEventListener('click', () =>{
   ventanaEmergenteTierra.classList.add('oculto');
 })
 
+// Marte
+
+
+const marte = document.querySelector('.mars');
+const ventanaEmergenteMarte = document.getElementById('ventana-emergente-marte')
+const cerrarVentanaMarte = document.getElementById('cerrar-ventana-marte')
+
+marte.addEventListener('click', () => {
+  ventanaEmergenteMarte.classList.remove('oculto')
+  ventanaEmergenteMarte.classList.add('ventana-emergente');
+ 
+})
+
+cerrarVentanaMarte.addEventListener('click', () =>{
+  ventanaEmergenteMarte.classList.add('oculto')
+}) 
+
+//Jupiter
+
+const jupiter = document.querySelector('.jupiter');
+const ventanaEmergenteJupiter = document.getElementById('ventana-emergente-jupiter')
+const cerrarVentanaJupiter = document.getElementById('cerrar-ventana-jupiter')
+
+jupiter.addEventListener('click', () => {
+  ventanaEmergenteJupiter.classList.remove('oculto')
+  ventanaEmergenteJupiter.classList.add('ventana-emergente');
+})
+
+cerrarVentanaJupiter.addEventListener('click', () => {
+  ventanaEmergenteJupiter.classList.add('oculto')
+})
+
+//Saturno
+
+const saturno = document.querySelector('.saturno');
+const ventanaEmergenteSaturno = document.getElementById('ventana-emergente-saturno')
+const cerrarVentanaSaturno = document.getElementById('cerrar-ventana-saturno');
+
+saturno.addEventListener('click', () => {
+  ventanaEmergenteSaturno.classList.remove('oculto')
+  ventanaEmergenteSaturno.classList.add('ventana-emergente');
+})
+
+cerrarVentanaSaturno.addEventListener('click',() => {
+  ventanaEmergenteSaturno.classList.add('oculto')
+})
+
+//Urano
+
+const urano = document.querySelector('.urano');
+const ventanaEmergenteUrano = document.getElementById('ventana-emergente-urano');
+const cerrarVentanaUrano = document.getElementById('cerrar-ventana-urano');
+
+urano.addEventListener('click', () => {
+  ventanaEmergenteUrano.classList.remove('oculto')
+  ventanaEmergenteUrano.classList.add('ventana-emergente');
 });
+
+cerrarVentanaUrano.addEventListener('click', () => {
+  ventanaEmergenteUrano.classList.add('oculto')
+})
+
+
+//Agradecimientos especiales
+
+const sirius = document.querySelector('.sirius');
+const ventanaEmergenteSirius = document.getElementById('ventana-emergente-sirius')
+const cerrarVentanaSirius = document.getElementById('cerrar-ventana-sirius')
+
+sirius.addEventListener('click', () => {
+  ventanaEmergenteSirius.classList.remove('oculto')
+  ventanaEmergenteSirius.classList.add('ventana-emergente');
+})
+
+cerrarVentanaSirius.addEventListener('click', () =>{
+  ventanaEmergenteSirius.classList.add('oculto')
+})
+
+const betelgeuse = document.querySelector('.betelgeuse');
+const ventanaEmergenteBetelgeuse = document.getElementById('ventana-emergente-betelgeuse')
+const cerrarVentanaBetelgeuse = document.getElementById('cerrar-ventana-betelgeuse')
+
+
+betelgeuse.addEventListener('click', () => {
+  ventanaEmergenteBetelgeuse.classList.remove('oculto')
+  ventanaEmergenteBetelgeuse.classList.add('ventana-emergente');
+})
+cerrarVentanaBetelgeuse.addEventListener('click', () =>{
+  ventanaEmergenteBetelgeuse.classList.add('oculto')
+})
+
+
+
+const aldebaran = document.querySelector('.aldebaran');
+const ventanaEmergenteAldebaran = document.getElementById('ventana-emergente-aldebaran');
+const cerrarVentanaAldebaran = document.getElementById('cerrar-ventana-aldebaran');
+
+aldebaran.addEventListener('click', () =>{
+  ventanaEmergenteAldebaran.classList.remove('oculto')
+  ventanaEmergenteAldebaran.classList.add('ventana-emergente');
+})
+
+cerrarVentanaAldebaran.addEventListener('click', () => {
+  ventanaEmergenteAldebaran.classList.add('oculto')
+})
+
+const rigel = document.querySelector('.rigel')
+const ventanaEmergenteRigel = document.getElementById('ventana-emergente-rigel');
+const cerrarVentanaRigel = document.getElementById('cerrar-ventana-rigel');
+
+rigel.addEventListener('click', () => {
+  ventanaEmergenteRigel.classList.remove('oculto');
+  ventanaEmergenteRigel.classList.add('ventana-emergente');
+
+  cerrarVentanaRigel.addEventListener('click', () => {
+    ventanaEmergenteRigel.classList.add('oculto')
+  })
+})
+
+});
+
 
 
 
